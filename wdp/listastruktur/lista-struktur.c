b43a8054f0b0a22b371d2ee97f8e7808c9ed5885
Gemmon <113787936+Gemmon@users.noreply.github.com>
2023-06-11 22:06:52 +0200

    Update lista-struktur.c

--- wdp/listastruktur/lista-struktur.c
+++ wdp/listastruktur/lista-struktur.c
@@ -1,8 +1,19 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
+
+int i,k,n,x;
+char name[20];
+int numb;
+int choice,numbUsers,size;
+
+
+
 struct user{
+    int numb;
     char name[20];
     int age;
+    /*
     int adult;
     int isAStudent;
     int studentID;
@@ -10,20 +21,71 @@ struct user{
     int weight;
     int height;
     int pet;
-
+*/
 };
 
-void addUser(struct user x, char name, int age ){
-      
-};
+void addUser(struct user *base, int *num){
+    *num = *num +1;
+    (struct user*)realloc(base,(*num)*sizeof(struct user));
+    printf("Podaj wiek: ");
+    scanf("%d", &base[*num-1].age);
+     printf("Podaj imie: ");
+    scanf("%s", &base[*num-1].name);
+}
+
+
+void showUser(struct user *base,int n){
+    printf(" Numer uzytkownika: %d\nImie: %d",base[n].age, base[n].name);    
+}
+
+void showUsers(struct user *base, int n) {
+
+    for (int i = 0; i < n; i++) {
+    printf("Numer: %d, Name: %s\n", base[i].age, base[i].name);
+    }
+}
 
 int main(){
-    struct user a;
-    strcpy( a.name, "Sam" );
-    a.age = 21;
+ 
+ int num = 2;
+ struct user *base;
+ base = malloc(sizeof(struct user)*num);
 
-    printf("wiek: %d \n", a.age);
-    printf("imie: %s \n", a.name);
-    //printf("wiek: %d \n", b.age);
+ int wart = 1;
+ base[0].age = 13;
+ strcpy(base[0].name,"Tamo");
     
-};
+    
+  printf("Wybierz z opcji:\n 1.Dodaj uzytkownika \n 2.Wyswietl wszystkich \n 3.Pokaz uzytkownika \n 4.Usun uzytkownika \n");
+  scanf("%d", &choice);
+
+    switch (choice){
+    case 1:
+      addUser(base, &num);  
+    break;
+
+    case 2:
+          
+      showUsers(base, num);
+
+      break;
+
+    case 3:
+
+    printf("ktorego sasma? \n");
+    scanf("%d", &numb);      
+    showUser(base, numb);
+    
+    break;
+    
+    case 4:
+  
+    break;
+
+    default:
+      printf("Wybrano nie poprawnie.");
+      break;
+    }
+    free(base);  
+    return 0;
+}
